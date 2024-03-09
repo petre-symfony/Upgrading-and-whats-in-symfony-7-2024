2b2145eb002b7809884564c0f49808710497e3d7
petrero <petrero@gmail.com>
2024-03-09 18:00:01 +0200

    9.2 . Then delete the import... and this comment while we're here. Replace axios.get() with just fetch(). Then, to see if this is working, console.log(response).
     Over in browser-land, smash that play button to trigger the method. Cool! The last two lines aren't working, but we see the response! It did make an Ajax call.

--- assets/controllers/song-controls_controller.js
+++ assets/controllers/song-controls_controller.js
@@ -1,16 +1,5 @@
 import {Controller} from '@hotwired/stimulus';
 
-/*
- * This is an example Stimulus controller!
- *
- * Any element with a data-controller="hello" attribute will cause
- * this controller to be executed. The name "hello" comes from the filename:
- * hello_controller.js -> "hello"
- *
- * Delete this file or adapt it for your use!
- */
-import axios from 'axios';
-
 export default class extends Controller {
   static values = {
     infoUrl: String
@@ -19,8 +8,9 @@ export default class extends Controller {
   play(event) {
     event.preventDefault();
 
-    axios.get(this.infoUrlValue)
+    fetch(this.infoUrlValue)
       .then((response) => {
+        console.log(response)
         const audio = new Audio(response.data.url);
         audio.play();
       });
