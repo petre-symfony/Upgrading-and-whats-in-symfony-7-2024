b022f92f556e0f39275298f478882e03bd361943
petrero <petrero@gmail.com>
2024-03-09 16:27:43 +0200

    8.6 Finally, remove the import from app.css and import it from app.js.
     Oh, and because we changed to the variable font, in app.css, update the font family to Roboto Condensed Variable.
    
     Over on the site, watch the font when I refresh. Got it! Grabbing those third party CSS files might be the trickiest thing you'll do in AssetMapper.
    
     Oh, and if you're using Sass or Tailwind, there are Symfonycasts bundles to support both of those in AssetMapper.

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,7 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import '@fontsource-variable/roboto-condensed/index.min.css';
 import './styles/app.css';
 
 // start the Stimulus application
