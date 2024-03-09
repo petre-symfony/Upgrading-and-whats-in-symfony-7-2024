e2da68f8e760e9dde70bd20ea68cb4ed17b249bb
petrero <petrero@gmail.com>
2024-03-09 16:54:39 +0200

    8.11 I could figure out what changed and fix this... But we can also downgrade. Update the version in importmap.php to 5.1.3.
     If we just did that and refreshed, nothing would change: the newer version is still downloaded into assets/vendor/. To sync that directory with importmap.php, run:
    
      php bin/console importmap:install
     Think of this is as the composer install of the AssetMapper world. It noticed that we changed two packages and downloaded those. And just like that, we've crossed the finish line! We're running AssetMapper!
    
     Next up, let's take three minutes to modernize & simplify our JavaScript.

--- importmap.php
+++ importmap.php
@@ -26,13 +26,13 @@ return [
         'version' => '7.3.0',
     ],
     'bootstrap' => [
-        'version' => '5.3.3',
+        'version' => '5.1.3',
     ],
     '@popperjs/core' => [
         'version' => '2.11.8',
     ],
     'bootstrap/dist/css/bootstrap.min.css' => [
-        'version' => '5.3.3',
+        'version' => '5.1.3',
         'type' => 'css',
     ],
     '@fortawesome/fontawesome-free/css/all.css' => [
