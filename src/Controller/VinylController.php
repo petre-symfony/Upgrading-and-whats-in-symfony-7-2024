252e64848092ecdbf46122c2fa86069818f1c8dc
petrero <petrero@gmail.com>
2024-03-09 18:48:24 +0200

    10.4 We want an iterable of every services that implement a specific tag. Grab those with #[AutowireIterator], then the tag we want: console.command.
     And just like that, we got them! We see all 102 console commands in my app.

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -7,6 +7,7 @@ use Pagerfanta\Doctrine\ORM\QueryAdapter;
 use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\DependencyInjection\Attribute\AutowireIterator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -16,6 +17,7 @@ class VinylController extends AbstractController {
 	public function __construct(
 		#[Autowire(param: 'kernel.debug')]
 		private bool $isDebug,
+		#[AutowireIterator('console.command')]
 		private iterable $commands
 	) {
 		foreach($this->commands as $command) {
