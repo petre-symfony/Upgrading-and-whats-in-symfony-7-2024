f23835e5f89a75fcf76767476069d963a7eb285e
petrero <petrero@gmail.com>
2024-04-04 21:11:30 +0300

    13.3 In LogHello, give it a constructor with public int $length. This will help us figure out which message is being handled and when. In the handler, also add a constructor so we can autowire LoggerInterface $logger.
     Down in the method, use $this->logger->warning() - just so these log entries are easy to see - then str_repeat() to log a guitar icon $message->length times. I'll also log that number at the end.
    
     Message & handler check!

--- src/Message/LogHello.php
+++ src/Message/LogHello.php
@@ -3,20 +3,6 @@
 namespace App\Message;
 
 final class LogHello {
-	/*
-	 * Add whatever properties and methods you need
-	 * to hold the data for this message class.
-	 */
-
-	//     private $name;
-
-	//     public function __construct(string $name)
-	//     {
-	//         $this->name = $name;
-	//     }
-
-	//    public function getName(): string
-	//    {
-	//        return $this->name;
-	//    }
+	public function __construct(public int $length) {
+	}
 }
--- src/MessageHandler/LogHelloHandler.php
+++ src/MessageHandler/LogHelloHandler.php
@@ -3,11 +3,15 @@
 namespace App\MessageHandler;
 
 use App\Message\LogHello;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 final class LogHelloHandler {
+	public function __construct(private LoggerInterface $logger) {
+	}
+
 	public function __invoke(LogHello $message) {
-		// do something with your message
+		$this->logger->warning(str_repeat('🎸', $message->length).' '.$message->length);
 	}
 }
