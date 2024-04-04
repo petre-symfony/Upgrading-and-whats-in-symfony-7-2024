e2b867163a37d7226cf405b22cc21b4560c61ee6
petrero <petrero@gmail.com>
2024-04-04 22:21:18 +0300

    13.5 Make your Schedule Stateful  - Though there is one problem hiding in plain sight: if we restart the command, it creates the schedule from scratch. That means that it waits a fresh new three seconds and four seconds before it dispatches the messages.
     In a real app, this will be a problem. Imagine you have a message that runs every seven days. For some reason, after 5 days, your messenger:consume command exits and is restarted. Because of this, your recurring message will now run seven days after this restart: so it will run on day 12. If it keeps getting restarted, your message may never run!
    
     This is not workable. And so, in the real world, we always make our schedule stateful. And this easy. Create a __construct method and autowire a private CacheInterface: the one from Symfony cache.
    
     Down below, call ->stateful() and pass $this->cache.
    
     Also, open services.yaml. In an earlier tutorial, I added some config that effectively disabled the cache in the dev environment. Remove that so we have a proper cache.
    
     Ok, stop the worker and restart it. The first time we do this, it's going to have the same behavior as before: wait three seconds and four seconds. There we go.
    
     But now, stop this, wait a few seconds and watch what happens when I restart. It catches up! Those messages happened immediately!
    
     The state keeps track of the last time Scheduler checked for messages. And so, if your worker gets turned off for a bit, when it restarts, it reads that time and uses it as its starting time so it can catch up with all the messages that it missed.
    
     It does mean that you may have some messages that are executed multiple times immediately, but it won't miss anything.
    
     Multiple Workers: Lock your Schedule
     - Oh, and if you plan to have multiple workers for your scheduler transport, you'll also need to add a lock to the schedule. This is easy and covered in the docs: autowire the lock factory, then call ->lock() to pass in a new lock. This will make sure that two workers don't grab the same recurring message at the same time and both process it.
    
     All right team, that's all I've got! Thanks for hanging out. If you have any questions about upgrading or hit a problem we didn't mention, we're here for you down in the comments. And let us know if you have a victory: we love hearing success.
    
     All right, friends. See you next time!

--- src/Scheduler/MainSchedule.php
+++ src/Scheduler/MainSchedule.php
@@ -7,13 +7,17 @@ use Symfony\Component\Scheduler\Attribute\AsSchedule;
 use Symfony\Component\Scheduler\RecurringMessage;
 use Symfony\Component\Scheduler\Schedule;
 use Symfony\Component\Scheduler\ScheduleProviderInterface;
+use Symfony\Contracts\Cache\CacheInterface;
 
 #[AsSchedule]
 class MainSchedule implements ScheduleProviderInterface {
+	public function __construct(private CacheInterface $cache) {
+	}
+
 	public function getSchedule(): Schedule {
 		return (new Schedule())->add(
 			RecurringMessage::every('4 seconds', new LogHello(4)),
 			RecurringMessage::every('3 seconds', new LogHello(3))
-		);
+		)->stateful($this->cache);
 	}
 }
